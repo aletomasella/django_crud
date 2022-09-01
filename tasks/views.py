516590724b626db9ad7291d2cbf0dfff1ad1a60d
Alejandro Tomasella <aletomasella09@gmail.com>
2022-09-01 12:56:31 -0300

    First Commit

---
+++ tasks/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from .models import Task
+
+# Create your views here.
+
+def list_task(request):
+    tasks = Task.objects.all()
+    return render(request, 'list_task.html', {'tasks': tasks})
+
+
+def create_task(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        Task.objects.create(title=title, description=description)
+    return redirect('/tasks/')
+
+
+def delete_task(request, id):
+    id = request.GET.get('id')
+    task = Task.objects.get(id=id)
+    task.delete()
+    return redirect('/tasks/')
+
+
