133b84f23fc1695f4c0d6851cf922bab0f336539
Alejandro Tomasella <aletomasella09@gmail.com>
2022-09-01 16:24:48 -0300

    Small Update

--- django_crud/settings.py
+++ django_crud/settings.py
@@ -78,8 +78,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'tasksdb',
-        'USER' : 'aletomasella',
-        'PASSWORD' : 'yitafiona',
+        'USER' : 'postgres',
+        'PASSWORD' : 'postgres',
         'HOST' : 'localhost',
         'PORT' : '5432',
     }
