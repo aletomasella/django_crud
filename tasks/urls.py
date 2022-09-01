3ded589fa491dc78cc904599deeff89f028f2618
Alejandro Tomasella <aletomasella09@gmail.com>
2022-09-01 16:11:12 -0300

    Adding boostrap

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import list_task, create_task, delete_task
+from .views import list_task, create_task, delete_task, update_task
 
 urlpatterns = [
     path('', list_task, name='list_task'),
     path('new/', create_task, name='create_task'),
     path('delete/<int:id>/', delete_task, name='delete_task'),
+    path('update/<int:id>/', update_task, name='update_task'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -21,4 +21,15 @@ def delete_task(request, id):
     task.delete()
     return redirect('/tasks/')
 
+def update_task(request, id):
+    task = Task.objects.get(id=id)
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        task.title = title
+        task.description = description
+        task.save()
+    return redirect('/tasks/')
+    # return render(request, 'update_task.html', {'task': task})
+
 
